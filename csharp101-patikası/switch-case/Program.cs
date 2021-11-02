fb5dbe4afc4250b546bfa087c13ef016a67f065a
Elif Yener <elif.yener@bil.omu.edu.tr>
2021-11-03 00:03:18 +0300

    Add "switch-case"

---
+++ csharp101-patikası/switch-case/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            //Expression
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındasınız!");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat ayındasınız!");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart ayındasınız!");
+                    break;
+                case 11:
+                    Console.WriteLine("Kasım ayındasınız!");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış veri girişi yaptınız");
+                    break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış mevsimindesiniz!");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar mevsimindesiniz!");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz mevsimindesiniz!");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar mevsimindesiniz!");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
