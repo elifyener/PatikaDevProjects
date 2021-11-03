c29a6286526e1b70167e3f83e462fe8daf60d2ae
Elif Yener <elif.yener@bil.omu.edu.tr>
2021-11-03 12:15:53 +0300

    Add "diziler"

---
+++ csharp101-patikası/diziler/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dizi Tanımlama
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = {"kedi", "köpek", "kuş", "maymun"};
+
+            int[] dizi;
+            dizi = new int[5];
+
+            // Dizilere Değer Atama ve Erişim
+            renkler[0] = "Mavi";
+            dizi[3] = 19;
+
+            Console.WriteLine(hayvanlar[0]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            // Döngüler Dizi Kullanımı
+            // Klavyeden girilen n tane sayının ortalamasını hesaplayan program
+            Console.Write("Lütfen dizinin eleman sayısını giriniz: ");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}. sayısını giriniz: ", i+1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+                toplam += sayi;
+            Console.WriteLine("Ortalama: " + toplam/diziUzunlugu);
+        }
+    }
+}
