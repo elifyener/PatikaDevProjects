905d1550a2f1d87203d8ed8ee0d8f7c0c3556f7b
Elif Yener <elif.yener@bil.omu.edu.tr>
2021-11-03 13:28:03 +0300

    Add "metotlar-overloading"

---
+++ csharp101-patikası/metotlar-overloading/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace metotlar_overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //out parametreler
+            string sayi = "999";
+            int outSayi;
+
+            bool sonuc = int.TryParse(sayi, out outSayi);
+
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı!");
+                Console.WriteLine(outSayi);
+            }
+            else{
+                Console.WriteLine("Başarısız!");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(4, 5, out int toplamSonuc);
+            Console.WriteLine(toplamSonuc);
+
+            // Metot Aşırı Yükleme - Overloading
+
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("elif", "yener");
+
+            //Metot İmzası
+            //metotAdi + parametre sayisi + parametre
+        }
+    }
+
+    class Metotlar
+    {
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(string veri1, string veri2)
+        {
+            Console.WriteLine(veri1 + veri2);
+        }
+    }
+}
