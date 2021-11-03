92a1b6976511c875e4b5781ecfc61dee9103cc60
Elif Yener <elif.yener@bil.omu.edu.tr>
2021-11-03 13:07:26 +0300

    Add "metotlar"

---
+++ csharp101-patikası/metotlar/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //erisim_belirteci geri_donustipi metot_adi(parametreListesi/arguman)
+            //{
+                //komutlar;
+                //return;
+            //}
+
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a+b);
+
+            int sonuc = Topla(a, b);
+            Console.WriteLine(a+b);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2 = ornek.ArttirveTopla(ref a, ref b);
+            ornek.EkranaYazdir(Convert.ToString(sonuc2));
+            ornek.EkranaYazdir(Convert.ToString(a + b));
+        }
+
+        static int Topla(int deger1, int deger2)
+        {
+            return(deger1 + deger2);
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttirveTopla(ref int deger1, ref int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+            return(deger1 + deger2);
+        }
+    }
+}
