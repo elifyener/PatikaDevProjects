c1c5babc643721bc3056ecb9dcaebe863fbddd9a
Elif Yener <elif.yener@bil.omu.edu.tr>
2021-11-04 14:09:05 +0300

    Add "static-sinif-ve-uyeler"

---
+++ csharp101-patikası/static-sinif-ve-uyeler/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace static_sinif_ve_uyeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+
+            Calisan calisan = new Calisan("Ayşe", "Yılmaz", "IK");
+            Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+
+            Calisan calisan2 = new Calisan("Ayşe", "Yılmaz", "IK");
+            Calisan calisan3 = new Calisan("Ayşe", "Yılmaz", "IK");
+            Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+
+            Console.WriteLine("Toplama İşlemi Sonucu: {0}", Islemler.Topla(100, 200));
+            Console.WriteLine("Toplama İşlemi Sonucu: {0}", Islemler.Cikar(400, 200));
+
+        }
+    }
+
+    class Calisan
+    {
+        private static int calisanSayisi;
+        public static int CalisanSayisi { get => calisanSayisi;}
+
+        private string Isim;
+        private string Soyisim;
+        private string Departman;
+
+        static Calisan(){
+            calisanSayisi = 0;
+        }
+        public Calisan(string isim, string soyisim, string departman)
+        {
+            this.Isim = isim;
+            this.Soyisim = soyisim;
+            this.Departman = departman;
+            calisanSayisi++;
+        }
+    }
+    static class Islemler
+    {
+        public static long Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public static long Cikar(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+    }
+}
