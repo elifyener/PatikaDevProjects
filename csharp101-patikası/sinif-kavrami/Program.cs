10bc6dfa345aa7f3abcc81e2fc0c9b82ce80cc53
Elif Yener <elif.yener@bil.omu.edu.tr>
2021-11-04 12:02:30 +0300

    Add "sinif-kavrami"

---
+++ csharp101-patikası/sinif-kavrami/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace sinif_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Söz Dizimi
+            // class SinifAdi
+            // {
+            //      [Erişim Belirleyici] [Veri Tipi] OzellikAdi;
+            //      [Erişim Belirleyici] [Geri Dönüş Tipi] MetotAdi[Parametre Listesi]
+            //      {
+            //          //Metot Komutlar
+            //      }
+            // }
+
+            // Erişim Belirleyiciler
+            // * Public
+            // * Private
+            // * Internal
+            // * Protected
+
+            Calisan calisan1 = new Calisan();
+            calisan1.Ad = "Ayşe";
+            calisan1.Soyad = "Kara";
+            calisan1.No = 123456;
+            calisan1.Departman = "İnsan Kaynakları";
+
+            calisan1.CalisanBilgileri();
+
+            Console.WriteLine("**********");
+
+            Calisan calisan2 = new Calisan();
+            calisan2.Ad = "Deniz";
+            calisan2.Soyad = "Arda";
+            calisan2.No = 234567;
+            calisan2.Departman = "Satın Alma";
+
+            calisan2.CalisanBilgileri();
+        }
+    }
+
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışanın Adı: {0}", Ad);
+            Console.WriteLine("Çalışanın Soyadı: {0}", Soyad);
+            Console.WriteLine("Çalışanın Numarası: {0}", No);
+            Console.WriteLine("Çalışanın Departmanı: {0}", Departman);
+        }
+    }
+}
