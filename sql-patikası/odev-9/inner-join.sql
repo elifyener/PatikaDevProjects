5c380fb8e9e59cd2abda038a2f4e10c2ff71907c
Elif Yener <elif.yener@bil.omu.edu.tr>
2022-01-13 13:02:04 +0300

    sql inner join

---
+++ sql-patikası/odev-9/inner-join.sql
@@ -0,0 +1,22 @@
+/*
+1. city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+2. customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+3. customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+*/
+
+/*
+SELECT city.city, country.country
+FROM city
+INNER JOIN country 
+ON city.country_id = country.country_id
+
+SELECT payment_id, first_name, last_name
+FROM customer
+INNER JOIN payment
+ON payment.customer_id = customer.customer_id
+
+SELECT rental.rental_id, customer.first_name, customer.last_name
+FROM customer
+INNER JOIN rental
+ON rental.customer_id = customer.customer_id
+*/
